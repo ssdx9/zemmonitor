37627258fb01583a17064c7eed6b30ac66197734
ssdx9 <ssdxmail@gmail.com>
2021-10-05 12:09:35 +0800

    comment code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#zemmonitor dash 1
+#zemmonitor dash for heroku
 import plotly.express as px
 from bs4 import BeautifulSoup
 import requests
@@ -9,15 +9,17 @@ from dash import html
 from datetime import datetime, timedelta
 import plotly.graph_objs as go
 
+# Блок необходимый для gunicorn
 app = dash.Dash(__name__)
 server = app.server
 
+# Парсинг данных со старого сайта
 url = "http://seis-bykl.ru/index.php?ma=1"
 r = requests.get(url)
 r.encoding = r.apparent_encoding # проверка содержимого на кодировку и переключение на неё
 soup = BeautifulSoup(r.text, "html.parser") # создаем whole-doc-объект soup
 
-# Скрапинг через ховеры:
+# Скрапинг через marker hovers:
 areas = soup.map.find_all('area') # достаем всё содержимое по тегу area
 df={'date':[],'time':[],'lat':[],'lon':[],'K':[],'Ks':[], 'affect':[]} # заготовка словаря под датафрейм
 for area in areas: #в каждом элементе списка    
@@ -31,13 +33,14 @@ for area in areas: #в каждом элементе списка
     Ks = int(((float(Ks[0])-8.6)**2)*10000) # подбираем размерность для size    
     NN = []
     NN.append(Ks)    
-    df['Ks'].extend(NN)
+    df['Ks'].extend(NN) # Ks нужен для регулировки размеров для scattermapbox, потому что size только int и str(?)
     ll = []
-    ll = re.findall(r"[А-Яа-я].+$", gottitle) # из-за того, что size только int и str
-    ll.extend([' '])
-    ls = ["".join(ll)]
+    ll = re.findall(r"[А-Яа-я].+$", gottitle) 
+    ll.extend([' ']) # кроме добавления значения в список (оно может быть none) добавляем еще пробел, чтобы хоть что-нибудь было
+    ls = ["".join(ll)] # таким образом будет либо пробел, либо значение с пробелом - и list будет сохранять необходимую размерность
     df['affect'].extend(ls)     
 
+# Отрисовка пустой карты
 fig=px.scatter_mapbox(
     center={'lat':54,'lon':109},   
     zoom=5,
@@ -45,20 +48,20 @@ fig=px.scatter_mapbox(
     ) 
 
 # Поочередное нанесение отдельного маркера с отдельной легендой
-for i in range(9,-1,-1):
-    dfdt=datetime.strptime((str(df['date'][i]) + ',' + str(df['time'][i])), "%Y-%m-%d,%H:%M:%S") 
-    dfdt=dfdt+timedelta(hours=8)
-    tddt=datetime.today().date()    
-    if i==0:
+for i in range(9,-1,-1): # в обратном порядке для того, чтобы последнее событие было на сверху (помещено на plot последним)
+    dfdt=datetime.strptime((str(df['date'][i]) + ',' + str(df['time'][i])), "%Y-%m-%d,%H:%M:%S") # расшифровка в формат datetime
+    dfdt=dfdt+timedelta(hours=8) # смещение в местное время
+    tddt=datetime.today().date() # переменная текущей даты
+    if i==0: # содержимое hover для последнего события (лучше переделать в template)
         textif='Последнее землетрясение<br>Дата и время местные: {} <br>Дата и время по Гринвичу: {} <br>Энергетический класс: {} <br>Координаты: {} {}<br>Затронутые населенные пункты: {}'.format(
-            (str(dfdt.date()) + ' ' + str(dfdt.strftime("%X"))),
-            (df['date'][i] + ' ' + df['time'][i]),
+            (str(dfdt.date()) + ' ' + str(dfdt.strftime("%X"))), # местное
+            (df['date'][i] + ' ' + df['time'][i]), # Гринвич
             df['K'][i],df['lat'][i],df['lon'][i],
             (df['affect'][i] if df['affect'][i] != ' ' else 'нет данных') ),
-    else:
+    else: # содержимое hover для остальных событий
         textif='Дата и время местные: {} <br>Дата и время по Гринвичу: {} <br>Энергетический класс: {} <br>Координаты: {} {}<br>Затронутые населенные пункты: {}'.format(
-            (str(dfdt.date()) + ' ' + str(dfdt.strftime("%X"))),
-            (df['date'][i] + ' ' + df['time'][i]),
+            (str(dfdt.date()) + ' ' + str(dfdt.strftime("%X"))), # местное
+            (df['date'][i] + ' ' + df['time'][i]), # Гринвич
             df['K'][i],df['lat'][i],df['lon'][i],
             (df['affect'][i] if df['affect'][i] != ' ' else 'нет данных') ), 
     fig.add_trace(go.Scattermapbox(
@@ -67,15 +70,18 @@ for i in range(9,-1,-1):
         mode='markers',
         text=textif,
         hoverinfo=('text'),
-        hoverlabel={'bgcolor':('black' if i!=0 else 'yellow'), },        
-        marker=go.scattermapbox.Marker(size=((float(df['K'][i])-8)*20), color=('red' if i!=0 else 'yellow' ), opacity=(0.5 if i!=0 else 0.9),),        
-        name= ('Сегодня' if tddt==dfdt.date() else (str(dfdt.day)+str(dfdt.strftime("%b"))) and 'Вчера' if tddt-timedelta(days=1)==dfdt.date() else (str(dfdt.day)+ " " +str(dfdt.strftime("%b")))) + " "  
+        hoverlabel={'bgcolor':('black' if i!=0 else 'yellow'), },  # выделенный цвет только для последнего события       
+        marker=go.scattermapbox.Marker(size=((float(df['K'][i])-8)*20), # регулировка размера для go.scattermabpox
+                                        color=('red' if i!=0 else 'yellow' ), 
+                                        opacity=(0.5 if i!=0 else 0.9),),
+        # блок нужно оптимизировать        
+        name=('Сегодня' if tddt==dfdt.date() else (str(dfdt.day)+str(dfdt.strftime("%b"))) and 'Вчера' if tddt-timedelta(days=1)==dfdt.date() else (str(dfdt.day)+ " " +str(dfdt.strftime("%b")))) + " "  
                 + str(dfdt.strftime("%X")) + " "
                 + " Класс: " 
                 + df['K'][i] + " ",
-))
+    ))
 
-
+# переменные цвета
 colors = {
     'background': '#111111',
     # 'background': '#708090',
@@ -83,25 +89,27 @@ colors = {
     # 'text': '#C0C0C0',
 }
 
+# для учета глобальных против локальных переменных сначала настройка страницы
 fig.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
     font_color=colors['text'],
-    margin=dict(t=20, b=20, l=20, r=20),
+    margin=dict(t=20, b=20, l=20, r=20), # отступ нужно сохранять для прокрутки экрана в мобильной версии в горизонтальной ориентации
 )
 
+# затем настройка элементов
+# настройки легенды
 fig.update_layout(
     legend_itemclick="toggleothers", legend_itemdoubleclick="toggle",
     legend_bgcolor="#000000", 
-    font=dict(family="Arial", size=14, color="#ffffff"), #шрифт в легенде    
+    font=dict(family="Arial", size=14, color="#ffffff"), 
     legend=dict(
-    yanchor="top",
-    y=0.99,
-    xanchor="left",
-    x=0.01),   
-    legend_traceorder="reversed",
+        yanchor="top", y=0.99,
+        xanchor="left", x=0.01),   
+    legend_traceorder="reversed", # обратный порядок, чтобы последнее размещаемое (поверх) событие было первым в легенде (как самое актуальное)
     )
 
+# макет страницы; требуется добавление стиля css (отдельный файл в директории) для настройки общего фона страницы в браузере
 app.layout = html.Div(
     style={'backgroundColor': colors['background']}, children=[
     html.H1(children='Монитор сейсмической активности на Байкале',
@@ -110,11 +118,9 @@ app.layout = html.Div(
         style={'textAlign': 'center', 'color': colors['text']}),
 
     dcc.Graph(figure=fig,
-    style = dict(height = 600),
-    # responsive=True,
-    # style={"min-height":"0","flex-grow":"1"},    
+    style = dict(height = 600), # найти средство автоподгонки высоты к высоте видимого экрана
     )
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == '__main__': # необходимо для heroku
+    app.run_server(debug=True) # необходимо для dash
